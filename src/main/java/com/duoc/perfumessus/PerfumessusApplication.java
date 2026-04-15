0320fd1d767a814c109f89626921c7f04bc341a8
MarshallVillalobos <mars.villalobos@duocuc.cl>
2026-04-15 14:47:01 -0400

    Primer commit del nuevo proyecto

---
+++ src/main/java/com/duoc/perfumessus/PerfumessusApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.perfumessus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumessusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumessusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/perfumessus/PerfumessusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.perfumessus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumessusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
