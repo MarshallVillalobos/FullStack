1696937fc3723720580aedec233c6132cae50205
MarshallVillalobos <mars.villalobos@duocuc.cl>
2026-04-16 12:07:10 -0400

    Clase Perfume terminada

--- src/main/java/com/duoc/perfumessus/model/Perfume.java
+++ src/main/java/com/duoc/perfumessus/model/Perfume.java
@@ -1,5 +1,16 @@
 package com.duoc.perfumessus.model;
 
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Perfume {
+
+    @Id
+    private Long id;
 
 }
