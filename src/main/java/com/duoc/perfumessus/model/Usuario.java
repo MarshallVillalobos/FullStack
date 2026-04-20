bb9f6b7a657f12bbe72e02d53154f9e349d2c0dd
MarshallVillalobos <mars.villalobos@duocuc.cl>
2026-04-20 12:17:37 -0400

    Creacion de Atributos de clase Usuario

--- src/main/java/com/duoc/perfumessus/model/Usuario.java
+++ src/main/java/com/duoc/perfumessus/model/Usuario.java
@@ -1,5 +1,47 @@
 package com.duoc.perfumessus.model;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    @Column(unique = true)
+    @Email(message = "Debe ser un email valido")
+    @NotBlank
+    private String email;
+
+    @NotBlank(message = "La clave es obligatoria")
+    private String password;
+
+    private String rol;
+
+    //@OneToMany(mappedBy = "usuario")
+    //private List<Pedido> pedidos;
+
+
 
 }
