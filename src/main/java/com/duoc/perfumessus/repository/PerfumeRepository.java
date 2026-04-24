6c4a3a4c4b416e9095f4405c6fe86f3ff3623975
MarshallVillalobos <mars.villalobos@duocuc.cl>
2026-04-24 14:36:57 -0400

    Creacion de PerfumeRepository ocupando JpaRepository para mayor eficiencia

---
+++ src/main/java/com/duoc/perfumessus/repository/PerfumeRepository.java
@@ -0,0 +1,16 @@
+package com.duoc.perfumessus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.duoc.perfumessus.model.Perfume;
+
+@Repository
+public interface PerfumeRepository extends JpaRepository<Perfume, Long>{
+    // Con JPaRepository tenemos el crud basico
+    //findAll() (Buscar todos).
+    //findById(Long id) (Buscar por ID).
+    //save(Perfume perfume) (Guardar o actualizar).
+    // deleteById(Long id) (Borrar por ID).
+    // Ya si requerimos de consultas más especificas agregamos el metodo más abajo 
+}
